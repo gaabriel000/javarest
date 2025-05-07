95da4d834c75dad60346ef652ade6c767b1ba384
Gabriel Roani <gabrielroani2@gmail.com>
2025-05-07 19:14:24 -0300

    initial commit

---
+++ src/main/java/br/com/roani/gabriel/javarest/JavarestApplication.java
@@ -0,0 +1,13 @@
+package br.com.roani.gabriel.javarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roani/gabriel/javarest/JavarestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roani.gabriel.javarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
